516012b1ddba243f771ec4afffe512d34b2deb08
bramzil <amzil.brahim@gmail.com>
2024-07-24 22:56:48 +0100

    add ft_door_ctl

---
+++ ft_door_ctl.c
@@ -0,0 +1,32 @@
+# include "cube.h"
+
+void ft_door_ctl(t_data *data)
+{
+    if (data->door.counter <= 0)
+    {
+        if (data->door.state == 'N')
+        {
+            if (10 < data->door.var)
+            {
+                data->door.var -= 1;
+                ft_clear_image(data->ddd__img);
+                ft_cast_rays(data);
+            }
+            else
+                data->door.state = 'O';
+        }
+        else if (data->door.state == 'S')
+        {
+            if (data->door.var < 32)
+            {
+                data->door.var += 1;
+                ft_clear_image(data->ddd__img);
+                ft_cast_rays(data);
+            }
+            else
+                data->door.state = 'C';
+        }
+        data->door.counter = 10;
+    }
+    data->door.counter--;
+}
